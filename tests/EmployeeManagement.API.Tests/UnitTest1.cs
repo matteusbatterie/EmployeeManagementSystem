e64dddcd5ea50527d93e537877bb28096746da1d
Kaue Batista da Silva <83407339+KaueB@users.noreply.github.com>
2024-12-21 13:31:46 -0300

    Create initial structure

---
+++ tests/EmployeeManagement.API.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManagement.API.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/EmployeeManagement.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManagement.Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
